f73fbaedc3f6259198d5c15939f311dabe8e73f1
Matei Busui <matei@busui.ro>
2025-01-22 10:42:45 +0200

    Removed old system artifacts

--- genver.sh
+++ genver.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-source $(aware-devtools.sh libpath)/genver.sh
-source $(aware-devtools.sh libpath)/debian.sh
+VERSION=${VERSION:-1.0.1}
 
-USERNAME="build-rules-repo" PASSWORD='HX{EMB),'"'"'!:d-_bN4q' pkg_list_http \
-	"https://deb.aware.ro/debian/dists/dev/main/binary-all/Packages.gz" build-rules |\
-	latest_version build-rules | pkg_version | inc_patch
+echo $VERSION
